03c2ace6b715cebe6f0d7a8a21936a26035eb42d
1090184468 <40384475+1090184468@users.noreply.github.com>
2018-06-29 08:34:30 +0800

    Add files via upload

--- courseDesign(perfect).cpp
+++ courseDesign(perfect).cpp
@@ -51,7 +51,7 @@ int read(Worker_Salary ws[])
     FILE *fp;
     if(( fp = fopen("gz.dat","rb"))==NULL)
     {
-        printf("文件不存在或出错！！\n");
+        printf("\t文件不存在或出错！！\n");
         getchar();
         exit(-1);
     }
@@ -125,7 +125,7 @@ void write(Worker_Salary ws[])
     FILE *fp;
     if((fp=fopen("gz.dat","wb"))==NULL)
     {
-        printf("文件不存在或出错！！\n");
+        printf("\t文件不存在或出错！！\n");
         getchar();
         exit(-1);
     }
@@ -137,7 +137,7 @@ void write(Worker_Salary ws[])
         p=(*p).next;
     }
     fclose(fp);
-    printf("保存成功!!!!!\n");
+    printf("\t保存成功!!!!!\n");
 }
 /*************************************************
 Function:    list                              // 函数名称
@@ -154,15 +154,10 @@ void list(Worker_Salary ws[])
     p=head;
     while((*p).next!=NULL)
     {   
-        printf("工号：%s  |  姓名：%s | 岗位工资：%.2f | ",(*p).ID,(*p).name,(*p).p_salary);
-        printf("薪级工资：%.2f | \n职务津贴：%.2f | 绩效工资：%.2f | 应发工资：%.2f | \n",(*p).p_salary1,(*p).p_salary2,(*p).p_salary3,(*p).s_salary);
-        printf("个人所得税：%.2f | 实发工资：%.2f\n",(*p).tax,(*p).real_salary);
-        //cout<<"工号："<<(*p).ID<<"  |  "<<"姓名："<<(*p).name<<"  |  "<<"岗位工资："<<(*p).p_salary<<"  |  ";
-        /*cout<<"薪级工资："<<(*p).p_salary1<<"  |  "<<'\n'<<"职务津贴："<<(*p).p_salary2<<"  |  ";
-        cout<<"绩效工资："<<(*p).p_salary3<<"  |  "<<"应发工资："<<(*p).s_salary<<"  |  "<<'\n';*/
-        /*cout<<"个人所得税："<<(*p).tax<<"  |  "<<"实发工资："<<(*p).real_salary<<"  |"<<endl;
-        cout<<"________________________________________________________________________________"<<endl;*/
-        printf("_______________________________________________________________________________\n\n");
+        printf("\t工号：%s  |  姓名：%s | 岗位工资：%.2f | ",(*p).ID,(*p).name,(*p).p_salary);
+        printf("薪级工资：%.2f | \n\t职务津贴：%.2f | 绩效工资：%.2f | 应发工资：%.2f | \n",(*p).p_salary1,(*p).p_salary2,(*p).p_salary3,(*p).s_salary);
+        printf("\t个人所得税：%.2f | 实发工资：%.2f\n",(*p).tax,(*p).real_salary);
+        printf("___________________________________________________________________________________\n\n");
         p=p->next;
     }
 }
@@ -183,20 +178,15 @@ void find(Worker_Salary ws[],char id[])
     {
         if(strcmp((*p).ID,id)==0)
         {
-            printf("工号：%s  |  姓名：%s | 岗位工资：%.2f | ",(*p).ID,(*p).name,(*p).p_salary);
-            printf("薪级工资：%.2f | \n职务津贴：%.2f | 绩效工资：%.2f | 应发工资：%.2f | \n",(*p).p_salary1,(*p).p_salary2,(*p).p_salary3,(*p).s_salary);
-            printf("个人所得税：%.2f | 实发工资：%.2f\n",(*p).tax,(*p).real_salary);
-            printf("_______________________________________________________________________________\n\n");
-            /*cout<<"工号："<<(*p).ID<<"  |  "<<"姓名："<<(*p).name<<"  |  "<<"岗位工资："<<(*p).p_salary<<"  |  ";
-            cout<<"薪级工资："<<(*p).p_salary1<<"  |  "<<'\n'<<"职务津贴："<<(*p).p_salary2<<"  |  ";
-            cout<<"绩效工资："<<(*p).p_salary3<<"  |  "<<"应发工资："<<(*p).s_salary<<"  |  "<<'\n';
-            cout<<"个人所得税："<<(*p).tax<<"  |  "<<"实发工资："<<(*p).real_salary<<"  |"<<endl;
-            cout<<"________________________________________________________________________________"<<endl;*/
+            printf("\n\t工号：%s  |  姓名：%s | 岗位工资：%.2f | ",(*p).ID,(*p).name,(*p).p_salary);
+            printf("薪级工资：%.2f | \n\t职务津贴：%.2f | 绩效工资：%.2f | 应发工资：%.2f | \n",(*p).p_salary1,(*p).p_salary2,(*p).p_salary3,(*p).s_salary);
+            printf("\t个人所得税：%.2f | 实发工资：%.2f\n",(*p).tax,(*p).real_salary);
+            printf("___________________________________________________________________________________\n\n");
             return;
         }
         p=p->next;
     }
-    printf("不存在此工号的职工,请确认你所输入的工号!!!\n");
+    printf("\t不存在此工号的职工,请确认你所输入的工号!!!\n");
 }
 /*************************************************
 Function:    grsds                             // 函数名称
@@ -274,26 +264,26 @@ void modify(Worker_Salary ws[],char id[])
     {
         if(strcmp((*p).ID,id)==0)
         {   
-            printf("请重新输入该职工的基本信息：\n");
-            printf("请输入职工工号：");
+            printf("\t请重新输入该职工的基本信息：\n");
+            printf("\t请输入职工工号：");
             scanf("%s",(*p).ID);
-            printf("请输入职工姓名：");
+            printf("\t请输入职工姓名：");
             scanf("%s",(*p).name);
-            printf("请输入职工岗位工资：");
+            printf("\t请输入职工岗位工资：");
             scanf("%f",&(*p).p_salary);
-            printf("请输入职工薪级工资：");
+            printf("\t请输入职工薪级工资：");
             scanf("%f",&(*p).p_salary1);
-            printf("请输入职工职务津贴：");
+            printf("\t请输入职工职务津贴：");
             scanf("%f",&(*p).p_salary2);
-            printf("请输入职工绩效工资：");
+            printf("\t请输入职工绩效工资：");
             scanf("%f",&(*p).p_salary3);
             grsds(p);
-            printf("修改成功!!!!!!\n");
+            printf("\t修改成功!!!!!!\n");
             return;
         }
         p=p->next;
     }
-    printf("不存在此工号的职工,请确认你所输入的工号!!!\n");
+    printf("\t不存在此工号的职工,请确认你所输入的工号!!!\n");
 }
 /*************************************************
 Function:    del                               // 函数名称
@@ -318,13 +308,13 @@ void del(Worker_Salary ws[],int &num,char id[])
     }
     if( (*p).next == NULL)
     {
-        printf("不存在此工号的职工,请确认你所输入的工号!!!\n");  
+        printf("\t不存在此工号的职工,请确认你所输入的工号!!!\n");  
         return;
     }
 
     char code;
 
-    printf("是否确认删除：（确认输入1）（取消输入非1）\n");
+    printf("\t是否确认删除：（确认输入1）（取消输入非1）");
     scanf("\n%c",&code);
     if( code == '1' )
     {    
@@ -341,11 +331,11 @@ void del(Worker_Salary ws[],int &num,char id[])
             p=p->next;
         }
         num=num-1;
-        printf("删除成功!!!!!!\n");
+        printf("\t删除成功!!!!!!\n");
     }
     else
     {
-        printf("已取消删除操作!!!!!\n");
+        printf("\t已取消删除操作!!!!!\n");
     }
 }
 /*************************************************
@@ -361,17 +351,17 @@ Return:无                                      // 函数返回值的说明
 void add(Worker_Salary ws[],int &num)
 {     
     char gonghao[10];
-    printf("请输入新增职工的基本信息：");
+    printf("\t请输入新增职工的基本信息：");
     while(true)
     {
-        printf("请输入职工工号：");
+        printf("\t请输入职工工号：");
         scanf("%s",gonghao);
         p=head;
         while((*p).next!=NULL)
         {
             if(strcmp((*p).ID,gonghao)==0)
             {
-                printf("此工号已经存在，请重新输入\n");
+                printf("\t此工号已经存在，请重新输入\n");
                 break;
             }
 			p=p->next;
@@ -379,15 +369,15 @@ void add(Worker_Salary ws[],int &num)
         if( (*p).next==NULL )
         {   
             strcpy(ws[data_num].ID,gonghao);
-            printf("请输入职工姓名：");
+            printf("\t请输入职工姓名：");
             scanf("%s",ws[data_num].name);
-            printf("请输入职工岗位工资：");
+            printf("\t请输入职工岗位工资：");
             scanf("%f",&ws[data_num].p_salary);
-            printf("请输入职工薪级工资：");
+            printf("\t请输入职工薪级工资：");
             scanf("%f",&ws[data_num].p_salary1);
-            printf("请输入职工职务津贴：");
+            printf("\t请输入职工职务津贴：");
             scanf("%f",&ws[data_num].p_salary2);
-            printf("请输入职工绩效工资：");
+            printf("\t请输入职工绩效工资：");
             scanf("%f",&ws[data_num].p_salary3);
             grsds(&ws[data_num]);
             num=num+1;
@@ -395,7 +385,7 @@ void add(Worker_Salary ws[],int &num)
             (*p).next=&ws[data_num];
             ws[data_num].pre=p;
             ws[data_num].next=NULL;
-            printf("新增成功！！！\n");
+            printf("\t新增成功！！！\n");
             break;
         }
         else 
@@ -412,23 +402,30 @@ int main(){
     char gonghao[10];
 
     while(true)
-    {
-
-        printf("-------------------------------------------------------------------------------\n\n\n");
-        printf("\t1.查询--2.修改--3.添加--4.删除--5.保存--6.浏览--7.退出");
-        printf("\n\n\n-------------------------------------------------------------------------------\n");
-        printf("请输入你所要操作的代码：");
+    {   
+        printf("\n\n\t### 欢迎使用广西民族大学软件与信息安全学院职工工资管理系统 ###\n\n\n");
+        printf("\t请选择 (1 - 7) :\n");
+        printf("\t======================================================================\n");
+        printf("\t|\t1. 查询职工工资记录                                          |\n");
+        printf("\t|\t2. 修改职工工资记录                                          |\n");
+        printf("\t|\t3. 添加职工工资记录                                          |\n");
+        printf("\t|\t4. 删除职工工资记录                                          |\n");
+        printf("\t|\t5. 保存数据到文件                                            |\n");
+        printf("\t|\t6. 浏览职工记录                                              |\n");
+        printf("\t|\t7. 退出系统                                                  |\n");
+        printf("\t======================================================================\n\n");
+        printf("\t你的选择是：");
         scanf("%d",&code);   //操作代码输入
         switch(code)
         {
             case 1:   //查询	       
-                printf("请输入你所查询职工工号：");
+                printf("\t请输入你所查询职工工号：");
                 scanf("%s",gonghao);
                 find(zggz,gonghao);
                 break;
 
             case 2:  //修改
-                printf("请输入你所修改的职工工号：");
+                printf("\t请输入你所修改的职工工号：");
                 scanf("%s",gonghao);
                 modify(zggz,gonghao);
                 key=false;
@@ -440,7 +437,7 @@ int main(){
                 break;
 
             case 4:   //删除
-                printf("请输入你所删除的职工工号：");
+                printf("\t请输入你所删除的职工工号：");
                 scanf("%s",gonghao);
                 del(zggz,num,gonghao);
                 key=false;
@@ -452,20 +449,21 @@ int main(){
                 break;
 
             case 6:    //浏览
+                printf("\n");
                 list(zggz);
                 break;
 
             case 7:      //退出
                 if(key)
                 {
-                    printf("退出操作!!!!\n");
+                    printf("\t退出操作!!!!\n");
                     return 0;
                 }
                 else      //没保存修改后的数据，进行的提示
                 {   
                     char save_code;    
-                    printf("数据修改，还没有保存文件中！！！\n");
-                    printf("不保存请输入1，进行保存重新操作输入非1\n");
+                    printf("\t数据修改，还没有保存文件中！！！\n");
+                    printf("\t不保存请输入1，进行保存重新操作输入非1\n");
                     scanf("\n%c",&save_code);
                     if( save_code == '1')
                     {
@@ -479,7 +477,7 @@ int main(){
                 }
             	
             default:
-                printf("你输入的代码不正确！！！ 请重新输入！！！\n");
+                printf("\t你输入的代码不正确！！！ 请重新输入！！！\n");
         }
     }
     return 0;
